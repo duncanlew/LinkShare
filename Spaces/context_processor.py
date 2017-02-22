406af6dc70639354e697ca990c9ab6cb05edae46
Jochem <jochem@degoede.email>
2017-02-22 21:39:39 +0100

    Fix bug in context_proc that required logged in user

--- Spaces/context_processor.py
+++ Spaces/context_processor.py
@@ -1,4 +1,7 @@
 def list_of_spaces(request):
     from .models import Space
     from django.db.models import Q
-    return {'spaces_list': Space.objects.filter(Q(owner=request.user) | Q(added_users__in=[request.user]))}
+    if request.user.is_authenticated:
+        return {'spaces_list': Space.objects.filter(Q(owner=request.user) | Q(added_users__in=[request.user]))}
+    else:
+        return {}
